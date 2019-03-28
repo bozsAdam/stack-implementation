4f673fc668bc0e2a9424e46a92faa18f46244e63
Adam Bozsoki <09.adam.b@gmail.com>
2019-03-28 18:03:15 +0100

    push and size are implemented

--- src/main/java/stack/Stack.java
+++ src/main/java/stack/Stack.java
@@ -1,5 +1,10 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Stack<T>{
 
     private T[] storage;
@@ -8,8 +13,21 @@ public class Stack<T>{
         storage = (T[])new Object[size];
     }
 
-
-    public T[] getStorage() {
-        return storage;
+    public void push(T t){
+        if(size() != storage.length){
+            for (int i = 0; i < storage.length ; i++) {
+                if(storage[i] == null){
+                    storage[i] = t;
+                    return;
+                }
+            }
+        }
     }
+
+    public int size(){
+        return Stream.of(storage).filter(Objects::nonNull)
+                .collect(Collectors.toCollection(ArrayList::new))
+                .size();
+    }
+
 }
